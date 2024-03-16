ac1b313d4d68c62acf170a049e110e516e94a74c
Tihhonix <tihhonix@gmail.com>
2024-03-16 23:54:54 +0400

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	Auto_Test/src/test/java/ar/soft/runner/BaseTest.java

--- Auto_Test/src/test/java/ar/soft/LibraryTest.java
+++ Auto_Test/src/test/java/ar/soft/LibraryTest.java
@@ -13,42 +13,70 @@ public class LibraryTest extends BaseTest {
     WebDriver driver = new ChromeDriver();
 
     @Test
-    public void libraryClick() throws InterruptedException {
+    public void libraryChecListCreate() throws InterruptedException {
 
         driver.get(URL);
+        Thread.sleep(2000);
         driver.manage().window().setSize(new Dimension(1820,1080));
         driver.findElement(By.xpath(INPUT_EMAIL)).sendKeys(EMAIL);
         driver.findElement(By.xpath(INPUT_PASSWORD)).sendKeys(PASSWORD);
         driver.findElement(By.xpath(BTN_PASSWORD)).click();
-        Thread.sleep(9000);
+        Thread.sleep(10500);
 
-//        кнопка подкатегории
         new LibraryPage(driver)
                 .libraryClick()
                 .buttonChecListCreateClick()
                 .inputChecListNameClick()
                 .inputChecListNameSent()
+                .buttonChecklistAddCategory()
                 .inputChecListPrimaryClick()
                 .inputChecListPrimarySent()
                 .createCategoryChecList()
                 .createCategoryChecListSent()
-                .primaryButtonChecListCreateClick();
+                .primaryButtonChecListCreateClick()
+                .buttonChecklistSubcategory()
+                .inputChecklistSubcateName()
+                .inputChecklistSubcateSent()
+                .inputChecklistCategorySubcateClick()
+                .inputChecklistCategorySubcateNameSent()
+                .buttonChecklistCategory()
+                .buttonChecklistAdd();
 
-//                .createUserClick()
-//                .userName("админ проекта")
-//                .userLastName("FamilyTest")
-//                .userEmail("mikha-g@bk.ru")
-//                .userEmailClick()
-//                .userTimezoneClick()
-//                .userTime("UTC + 00:00")
-//                .userTimeEnter()
-//                .userRolesClick()
-//                .userRoles()
-//                .userPassword("zxcvbnm123")
-//                .userPasswordClick()
-//                .buttonSave();
-        Thread.sleep(5000);
         driver.quit();
     }
+
+    @Test
+    public void libraryChecListDelete() throws InterruptedException {
+
+        driver.get(URL);
+        Thread.sleep(2000);
+        driver.manage().window().setSize(new Dimension(1820,1080));
+        driver.findElement(By.xpath(INPUT_EMAIL)).sendKeys(EMAIL);
+        driver.findElement(By.xpath(INPUT_PASSWORD)).sendKeys(PASSWORD);
+        driver.findElement(By.xpath(BTN_PASSWORD)).click();
+        Thread.sleep(10500);
+
+        new LibraryPage(driver)
+                .libraryClick()
+                .buttonChecListCreateClick()
+                .inputChecListNameClick()
+                .inputChecListNameSent()
+                .buttonChecklistAddCategory()
+                .inputChecListPrimaryClick()
+                .inputChecListPrimarySent()
+                .createCategoryChecList()
+                .createCategoryChecListSent()
+                .primaryButtonChecListCreateClick()
+                .buttonChecklistCategoryDelete()
+                .buttonChecklistCategoryDeleteClick();
+//                .buttonChecklistSubcategory();
+
+
+
+//                .buttonChecklistAdd();
+
+//        driver.quit();
+    }
+
 
 }
