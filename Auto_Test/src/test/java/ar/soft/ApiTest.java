a1ab7b1fa80902c40ba56ffe376879f3ca7be6a8
Tihhonix <tihhonix@gmail.com>
2024-03-20 15:32:32 +0400

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	Auto_Test/src/test/java/ar/soft/runner/BaseTest.java

---
+++ Auto_Test/src/test/java/ar/soft/ApiTest.java
@@ -0,0 +1,35 @@
+package ar.soft;
+
+import org.junit.Test;
+
+import java.io.Closeable;
+import java.net.http.HttpClient;
+
+public class ApiTest {
+
+    private static class Pokemon {
+
+        public String name;
+        public String url;
+        public Pokemon(String name, String url) {
+           this.name = name;
+           this.url = url;
+        }
+    }
+
+    public Pokemon() {
+    }
+
+ private static class Pokemons {
+     public int count;
+     public String next;
+     public String previous;
+ }
+    @Test
+    public void httpTest() IOExctption, ParseExctption {
+        try {
+            CloseableHttpClient = HttpClient
+
+        }
+    }
+}
