06ee60cf3364f3cb225118a324a549139577b149
Evgeniy <yndrab@yandex.ru>
2024-03-28 12:44:12 +0400

    Merge remote-tracking branch 'origin/main4' into main4

--- Auto_Test/src/test/java/ar/soft/LogFileTest.java
+++ Auto_Test/src/test/java/ar/soft/LogFileTest.java
@@ -3,8 +3,13 @@ package ar.soft;
 import ar.soft.modelPage.UserPage;
 import ar.soft.runner.BaseTest;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 
 public class LogFileTest extends BaseTest {
 
@@ -17,5 +22,61 @@ public class LogFileTest extends BaseTest {
                 .URL();
     }
 
+    public static final String URL = "http://23.105.246.172:5000/login";
+    public static final String INPUT_PASSWORD = "//input[@class='ant-input']";
+    public static final String INPUT_EMAIL = "//input[@class='ant-input primaryInput  not-entered']";
+    public static final String BTN_PASSWORD = "//button[@class='ant-btn ant-btn-default authButton big colorPrimary ']";
+    public static final String EMAIL = "n-k-65@list.ru";
+    private final By getPaswordText = By.xpath("//h2[@class='ant-typography h2_m RestorePassword__sendSuccess-text'][contains(.,'Мы отправили по адресу')]");
 
+    private final By getErrorText = By.xpath("//div[@style='text-align: center; margin-bottom: 20px; color: rgb(255, 0, 0);']");
+
+
+
+    @BeforeTest
+    public void initWebDriver() {
+//        driver = new ChromeDriver();
+        driver.manage().window().setSize(new Dimension(1820,1080));
+    }
+
+    @AfterTest
+    public void closeWebDriver() {
+        driver.quit();
+    }
+    @org.testng.annotations.Test
+    public void removePaswordTest() throws InterruptedException {
+
+        driver.get(URL);
+
+        Thread.sleep(1000);
+
+        driver.findElement(By.xpath(INPUT_EMAIL)).sendKeys("yyyyyyyyyy@mail.xx");
+
+        driver.findElement(By.xpath(BTN_PASSWORD)).click();
+        Thread.sleep(1000);
+
+        String getError = driver.findElement(getErrorText).getText();
+
+        Assert.assertEquals(getError,"Неправильный логин или пароль");
+
+    }
+
+    @org.testng.annotations.Test
+    public void regaTest() throws InterruptedException {
+
+        driver.get(URL);
+        Thread.sleep(1000);
+
+        driver.findElement(By.xpath("//h2[@class='ant-typography h2_m Login__restore-text']")).click();
+
+        driver.findElement(By.xpath(INPUT_EMAIL)).sendKeys(EMAIL);
+
+        driver.findElement(By.xpath(BTN_PASSWORD)).click();
+        Thread.sleep(1000);
+
+        String getPasError = driver.findElement(getPaswordText).getText();
+
+        Assert.assertEquals(getPasError,"Мы отправили по адресу n-k-65@list.ru ссылку для восстановления доступа");
+
+    }
 }
