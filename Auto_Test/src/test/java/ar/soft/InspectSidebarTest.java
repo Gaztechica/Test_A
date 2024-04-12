016974919e54215e604f89b281d721cbe075e21f
Evgeniy <yndrab@yandex.ru>
2024-04-12 15:23:04 +0400

    Merge remote-tracking branch 'origin/main4' into main4

---
+++ Auto_Test/src/test/java/ar/soft/InspectSidebarTest.java
@@ -0,0 +1,8 @@
+package ar.soft;
+
+import ar.soft.runner.BaseTest;
+
+public class InspectSidebarTest extends BaseTest {
+
+
+}
--- Auto_Test/src/test/java/ar/soft/modelPage/RemarkSidebarPage.java
+++ Auto_Test/src/test/java/ar/soft/modelPage/RemarkSidebarPage.java
@@ -1,11 +1,12 @@
 package ar.soft.modelPage;
 
+import ar.soft.modelPage.base.BasePage;
 import ar.soft.runner.BaseTest;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class RemarkSidebarPage extends BaseTest {
+public class RemarkSidebarPage extends BasePage {
 
     @FindBy(xpath = "//div[@class='ant-typography ant-typography-ellipsis ant-typography-single-line ant-typography-ellipsis-single-line p_r'][contains(.,'1Новый проект')]")
     private WebElement homePage;
@@ -13,6 +14,7 @@ public class RemarkSidebarPage extends BaseTest {
     public RemarkSidebarPage(WebDriver driver) {
         super(driver);
     }
+
 
     public RemarkSidebarPage homePage() {
         homePage.click();
