72fbce6aff80e711fbdf1dada18a379fd68cadf8
Evgeniy <yndrab@yandex.ru>
2024-04-12 15:55:24 +0400

    Merge remote-tracking branch 'origin/main4' into main4

--- Auto_Test/src/test/java/ar/soft/RemarcSidebarTest.java
+++ Auto_Test/src/test/java/ar/soft/RemarcSidebarTest.java
@@ -3,6 +3,7 @@ package ar.soft;
 import ar.soft.modelPage.RemarkSidebarPage;
 import ar.soft.runner.BaseTest;
 import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class RemarcSidebarTest extends BaseTest {
@@ -19,21 +20,16 @@ public class RemarcSidebarTest extends BaseTest {
                 .btnAddRemark()
                 .inputNameRemark()
                 .btnAddNameRemark();
-//        driver.findElement(By.xpath("//div[@class='ant-typography ant-typography-ellipsis ant-typography-single-line ant-typography-ellipsis-single-line p_r'][contains(.,'1Новый проект')]")).click();
-//        driver.findElement(By.xpath("//span[@class='ant-menu-title-content'][contains(.,'Замечания')]")).click();
-//        driver.findElement(By.xpath("//button[@class='ant-btn ant-btn-default primaryButton big colorPrimary ']")).click();
-//
-//        driver.findElement(By.xpath("//input[@class='ant-input primaryInput  not-entered']")).sendKeys("Замечаниe");
-//
-//        driver.findElement(By.xpath("//button[@class='ant-btn ant-btn-primary primaryButton big colorPrimary '][contains(.,'Создать')]")).click();
-//        driver.findElement(By.xpath("//span[@class='ant-menu-title-content'][contains(.,'Замечания')]")).click();
-//        driver.findElement(By.xpath("//span[@class='ant-menu-title-content'][contains(.,'Замечания')]")).click();
-//        getDriver().findElement(By.xpath(INPUT_EMAIL)).sendKeys("yyyyyyyyyy@mail.xx");
-//        getDriver().findElement(By.xpath(BTN_PASSWORD)).click();
-//
-//        String getError = getDriver().findElement(GET_ERROR).getText();
-//
-//        Assert.assertEquals(getError,"Неправильный логин или пароль");
+
+//        String getError = getDriver().findElement(By.xpath(
+//        "//div[@class='ant-typography ant-typography-ellipsis
+//        ant-typography-single-line ant-typography-ellipsis-single-line
+//        p_r']")).getText();
+
+        Assert.assertEquals(getDriver().
+                findElement(By.xpath("//div[@class='ant-typography " +
+                        "ant-typography-ellipsis ant-typography-single-line " +
+                        "ant-typography-ellipsis-single-line p_r']")).getText(),"Замечаниe");
     }
 
 
