a3e188eaa0301fe9ee4123971f9bd0aec508930b
Evgeniy <yndrab@yandex.ru>
2025-11-04 11:06:06 +0400

    fix InspectSidebarTest

--- Auto_Test/src/test/java/ar/soft/InspectSidebarTest.java
+++ Auto_Test/src/test/java/ar/soft/InspectSidebarTest.java
@@ -10,9 +10,10 @@ import java.time.LocalDate;
 public class InspectSidebarTest extends BaseTest {
 
     public final static String NAME_DRAFT_INSPECT = "черновик инспекции";
+    public final static String NAME_DRAFT_INSPECT_FILLED = "черновик инспекции заполнены все поля";
+    public final static String NAME_OT_INSPECT_FILLED = "ОТ, ПБ и ООС";
     public final static String NAME_RENAME_INSPECT = "переименовал инспекцию ";
     public final static String TAPE_WORK_INSPECT = "новый вид работ";
-    public final static String TAPE_WORK_INSPECT2 = "А-1 Монтаж системы отопления";
     public final static String ORGANIZATION_INSPECT = "TEST!1";
 
     @Test(priority = 1,
@@ -30,14 +31,14 @@ public class InspectSidebarTest extends BaseTest {
     }
 
     @Test(priority = 2,
-            description = "переименовать инспекцию")
+            description = "переименовать черновик инспекции")
     public void renameInspect() throws InterruptedException {
         String renameInspects = new InspectSidebarPage(getDriver())
                 .project()
                 .inspectSidebarClick()
                 .selectInspect()
                 .inspectRename(NAME_RENAME_INSPECT + LocalDate.now())
-                .getMessageCreateDraftInspectText();
+                .getRenameDraftInspectText();
 
         Assert.assertEquals(renameInspects, NAME_RENAME_INSPECT + LocalDate.now());
     }
@@ -49,16 +50,17 @@ public class InspectSidebarTest extends BaseTest {
                 .project()
                 .inspectSidebarClick()
                 .createInspectClick()
-                .inspectName(NAME_DRAFT_INSPECT)
+                .inspectName(NAME_DRAFT_INSPECT_FILLED)
                 .btnCreateInspectClick()
-                .addLocationClick()
                 .inspectTypesWork(TAPE_WORK_INSPECT)
                 .inspectOrganization(ORGANIZATION_INSPECT)
+                .inspectTypeControl()
                 .addChecklistClick()
                 .checkChecklistClick()
-                .getMessageCreateDraftInspectText();
+                .addLocationClick()
+                .getRenameDraftInspectText();
 
-        Assert.assertEquals(createInspect, "Черновик успешно создан");
+        Assert.assertEquals(createInspect, NAME_DRAFT_INSPECT);
     }
 
     @Test(priority = 4,
@@ -72,4 +74,43 @@ public class InspectSidebarTest extends BaseTest {
                 .btnSetDeleteInspectClick();
     }
 
+    @Test(priority = 5,
+            description = "опубликовать черновик инспекции")
+    public void filledInspect() throws InterruptedException {
+        String createDraftInspect = new InspectSidebarPage(getDriver())
+                .project()
+                .inspectSidebarClick()
+                .filledInspect(NAME_DRAFT_INSPECT_FILLED)
+                .btnFilledInspect()
+
+                .getMessageFilledDraftInspectText();
+
+        Assert.assertEquals(createDraftInspect, "Черновик успешно создан");
+    }
+
+    @Test(priority = 6,
+            description = "Завершить инспекцию")
+    public void filledDraftInspect() throws InterruptedException {
+        String createDraftInspect = new InspectSidebarPage(getDriver())
+                .project()
+                .inspectSidebarClick()
+                .selectCompleteInspect(NAME_OT_INSPECT_FILLED)
+                .btnCompleteInspect()
+                .getMessageFilledInspectText();
+
+        Assert.assertEquals(createDraftInspect, "Инспекция завершена");
+    }
+
+    @Test(priority = 7,
+            description = "Вернуть в работу инспекцию")
+    public void backWorkInspect() throws InterruptedException {
+        String createDraftInspect = new InspectSidebarPage(getDriver())
+                .project()
+                .inspectSidebarClick()
+                .selectCompleteInspect(NAME_OT_INSPECT_FILLED)
+                .btnBackWorkInspect()
+                .getMessageFilledInspectsText();
+
+        Assert.assertEquals(createDraftInspect, "Завершить");
+    }
 }
